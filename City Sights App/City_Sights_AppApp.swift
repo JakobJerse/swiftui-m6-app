9ecd89d937cf48ac4d3c2b5da1c00b70ef9326b1
JakobJerse <jakob.jerse@gmail.com>
2021-08-31 12:27:00 +0200

    set up the project

--- City Sights App/City_Sights_AppApp.swift
+++ City Sights App/City_Sights_AppApp.swift
@@ -1,5 +1,5 @@
 //
-//  City_Sights_AppApp.swift
+//  City_Sights_App.swift
 //  City Sights App
 //
 //  Created by Jakob Jerse on 31/08/2021.
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct City_Sights_AppApp: App {
+struct City_Sights_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
