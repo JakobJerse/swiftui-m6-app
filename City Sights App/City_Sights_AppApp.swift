063f0a38939d11c20745cfb74e7140554f31bf7a
JakobJerse <jakob.jerse@gmail.com>
2021-08-31 12:23:03 +0200

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Jakob Jerse on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
